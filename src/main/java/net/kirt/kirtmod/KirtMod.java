13860f83212826f1d7c4a4db063e799544eeb1e0
Kirtails <hajigraznajka@gmail.com>
2023-01-03 15:05:40 +0200

    among us

--- src/main/java/net/kirt/kirtmod/KirtMod.java
+++ src/main/java/net/kirt/kirtmod/KirtMod.java
@@ -1,18 +1,19 @@
-package com.example.example_mod;
+package net.kirt.kirtmod;
 
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class KirtMod implements ModInitializer {
+	public static final String MOD_ID = "kirtmod";
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod name as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("Example Mod");
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize(ModContainer mod) {
-		LOGGER.info("Hello Quilt world from {}!", mod.metadata().name());
+
 	}
 }
